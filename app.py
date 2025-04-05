2be696271c39ad567042b4bb082240db52ba39bf
Ritish <ritishmahajan15f@gmail.com>
2025-04-06 01:12:58 +0530

    new version 2.1

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def handle_webrtc_candidate(data):
                   room=room_code, include_self=False)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True,host="0.0.0.0",port="5000")
